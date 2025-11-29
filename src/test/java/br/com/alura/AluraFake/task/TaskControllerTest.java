5265866779be5ff0a33c3dd6d837703b87b0391f
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-11-28 23:34:14 -0300

    fix: removing unused imports on tests

--- src/test/java/br/com/alura/AluraFake/task/TaskControllerTest.java
+++ src/test/java/br/com/alura/AluraFake/task/TaskControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,8 +31,6 @@ class TaskControllerTest {
     void newOpenTextExercise__should_create_open_text_task_successfully() throws Exception {
         User instructor = new User("Paulo Silva", "paulo@alura.com.br", Role.INSTRUCTOR);
         Course course = new Course("Java Fundamentals", "Learn Java basics", instructor);
-
-
 
         OpenTextTaskDTO openTextTaskDTO = new OpenTextTaskDTO(1L, "What you think about statement?", 1);
 
--- src/test/java/br/com/alura/AluraFake/task/service/TaskServiceTest.java
+++ src/test/java/br/com/alura/AluraFake/task/service/TaskServiceTest.java
@@ -18,7 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
