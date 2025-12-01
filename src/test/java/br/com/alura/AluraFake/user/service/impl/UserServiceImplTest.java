a257335e53cee085e0e24f6267e1a7ddad2d9641
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-12-01 10:53:10 -0300

    test: capture saved user and assert persisted fields instead of instance equality

--- src/test/java/br/com/alura/AluraFake/user/service/impl/UserServiceImplTest.java
+++ src/test/java/br/com/alura/AluraFake/user/service/impl/UserServiceImplTest.java
@@ -10,10 +10,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Collections;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
